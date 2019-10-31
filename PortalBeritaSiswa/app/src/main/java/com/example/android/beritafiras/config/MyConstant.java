6e1b0150bdd0671944952dc1481e9608a5dba802
kutipduwa <mpeinc@rbs.sch.id>
2019-10-31 16:52:58 +0700

    First Commit Project Native Android for Development #31102019 #1652

---
+++ PortalBeritaSiswa/app/src/main/java/com/example/android/beritafiras/config/MyConstant.java
@@ -0,0 +1,27 @@
+package com.example.android.beritafiras.config;
+
+/**
+ * Created by Firas Luthfi on 2/11/2018.
+ */
+
+public class MyConstant {
+//    public static final String BASE_URL = "http://192.168.137.1/myapi/";
+//    public static final String FOTO_URL = "http://192.168.137.1/myapi/foto_berita/";
+
+//    public static final String BASE_URL = "http://192.168.43.49:8080/myapi/";
+//    public static final String THUMBNAILS_URL = "http://192.168.43.49:8080/myapi/thumbnails/";
+//    public static final String FOTO_URL = "http://192.168.43.49:8080/myapi/foto_berita/";
+
+//    public static final String BASE_URL = "http://192.168.1.36/api/portalberita/v1/";
+//    public static final String THUMBNAILS_URL = "http://192.168.1.36/api/portalberita/v1/thumbnails/";
+//    public static final String FOTO_URL = "http://192.168.1.362/api/portalberita/v1/foto_berita/";
+
+    public static final String BASE_URL = "http://192.168.1.19/api/portalberita-api/v3/";
+    public static final String THUMBNAILS_URL = "http://192.168.1.19/api/portalberita-api/v3/thumbnails/";
+    public static final String FOTO_URL = "http://192.168.1.19/api/portalberita-api/v3/gambar_berita/";
+
+    // VERSION
+    public static final String VERSION = "1.0.0";
+
+    // CEK
+}
